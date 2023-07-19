e7dd9d5e5e6eb1cd49f9ea92a26055b5ce64f25b
Simon Walker <simon.walker@localstack.cloud>
2023-07-19 11:34:29 +0100

    Implement finding localstack container

--- dockerdebug/__main__.py
+++ dockerdebug/__main__.py
@@ -1,11 +1,12 @@
 import socket
 import ssl
+from typing import Iterable, cast
 
 import dns.resolver
 import dns.rdatatype
 import dns.rrset
-from dnslib.dns import QTYPE, DNSQuestion, DNSRecord
-import docker
+from docker import DockerClient
+from docker.models.containers import Container
 
 
 TEST_DNS_NAMES = [
@@ -50,25 +51,50 @@ def resolve_name(name: str) -> dns.rrset.RRset | NoDomain:
         return NoDomain(name)
 
 
+class CannotFindLocalStackContainer(Exception):
+    pass
+
+
+class MultipleLocalStackContainerCandidates(Exception):
+    def __init__(self, candidates: set[str]):
+        self.candidates = candidates
+
+
+def _containers_with_localstack_labels(containers: Iterable[Container]) -> set[str]:
+    candidates = set()
+    for container in containers:
+        if container.labels.get("authors") == "LocalStack Contributors":
+            candidates.add(container.id)
+            continue
+    return candidates
+
+
+def _containers_with_exposed_ports(containers: Iterable[Container]) -> set[str]:
+    candidates = set()
+    for container in containers:
+        if "4566/tcp" in container.ports:
+            candidates.add(container.id)
+    return candidates
+
+
+def find_localstack_container(containers: Iterable[Container]) -> str:
+    candidates: set[str] = set()
+    candidates = candidates.union(_containers_with_localstack_labels(containers))
+    candidates = candidates.union(_containers_with_exposed_ports(containers))
+    if len(candidates) == 1:
+        return list(candidates)[0]
+    elif len(candidates) == 0:
+        raise CannotFindLocalStackContainer()
+    else:
+        raise MultipleLocalStackContainerCandidates(candidates)
+
+
 if __name__ == "__main__":
     print("Starting")
 
-    client = docker.DockerClient()
-    containers = client.containers
+    client = DockerClient()
+    containers = cast(list[Container], client.containers.list())
+    ls_container_id = find_localstack_container(containers)
+    print(f"Found local stack container id: {ls_container_id}")
 
     print("Done")
-    raise SystemExit(0)
-
-    q = DNSRecord(q=DNSQuestion("amazonaws.com", QTYPE.A))
-    a = DNSRecord.parse(q.send("8.8.8.8", 53, tcp=False))
-    for rr in a.rr:
-        print(rr.rdata)
-
-    print(socket.gethostbyname_ex("amazonaws.com"))
-
-    raise SystemExit(0)
-
-
-    verify_ssl_certificate("localhost", 9000)
-    # for name in TEST_DNS_NAMES:
-    #     print(resolve_name(name))
