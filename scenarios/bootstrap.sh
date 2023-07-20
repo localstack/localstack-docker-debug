a5fc26b3aec028b83a58c1b14235828c0e94830f
Simon Walker <simon.walker@localstack.cloud>
2023-07-20 14:35:58 +0100

    Bootstrap chdirs to the scenarios dir

--- scenarios/bootstrap.sh
+++ scenarios/bootstrap.sh
@@ -5,6 +5,10 @@
 set -euo pipefail
 set -x
 
+ROOT_DIR=$(dirname $(readlink -f $0))
+pushd $ROOT_DIR >/dev/null
+trap "popd >/dev/null" EXIT
+
 for scenario in 1-missing-network-configuration 2-no-subdomain-support 3-urls-with-localhost; do
     (cd $scenario/cdk && npm install)
 done
