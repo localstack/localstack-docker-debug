f6c499a2329b77cae28a6178dc2c957007e1eeb9
Simon Walker <simon.walker@localstack.cloud>
2023-07-24 14:54:31 +0100

    Send docker build output to stderr

--- probe.sh
+++ probe.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 IMAGE_NAME=simonrw/debug
 
 # build docker container
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME . >&2
 
 # run docker container
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME probe $*
